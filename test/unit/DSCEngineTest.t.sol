e7e04c7d3586b4b689ef4aad031c4de96c4779ad
Joshkovu <joashkuteesa223@gmail.com>
2026-01-30 08:44:48 +0300

    feat: Add test logic into the DSCEngine test file

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -1,29 +1,50 @@
 // SPDX-License-Identifier:MIT
 
-// Layout of Contract:
-// license
-// version
-// imports
-// errors
-// interfaces, libraries, contracts
-// Type declarations
-// State variables
-// Events
-// Modifiers
-// Functions
-
-// Layout of Functions:
-// constructor
-// receive function (if exists)
-// fallback function (if exists)
-// external
-// public
-// internal
-// private
-// internal & private view & pure functions
-// external & public view & pure functions
-
 pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
+import {DeployDsc} from "script/DeployDsc.s.sol";
+import {DecentralizedStableCoin} from "src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "src/DSCEngine.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+
+contract DSCEngineTest is Test {
+    DeployDsc deployer;
+    DecentralizedStableCoin dsc;
+    DSCEngine engine;
+    HelperConfig config;
+    address ethUsdPriceFeed;
+    address weth;
+    address private USER = makeAddr("User");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+
+    function setUp() public {
+        deployer = new DeployDsc();
+        (dsc, engine, config) = deployer.run();
+        (ethUsdPriceFeed, , weth, , ) = config.activeNetworkConfig();
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
+    }
+
+    ////////////////
+    //Price Tests //
+    ///////////////
+    function testGetUsdValue() public view {
+        uint256 ethAmount = 15e18;
+        uint256 expectedUsd = 30000e18;
+        uint256 actualUsd = engine.getUsdValue(weth, ethAmount);
+        assertEq(actualUsd, expectedUsd);
+    }
+
+    ///////////////////////////
+    //Deposit collateral Tests //
+    ////////////////////////////
+    function testCollateralRevertsIfZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(engine), AMOUNT_COLLATERAL);
+        vm.expectRevert(DSCEngine.DSCEngine__MustBeMoreThanZero.selector);
+        engine.depositCollateral(weth, 0);
+        vm.stopPrank();
+    }
+}
